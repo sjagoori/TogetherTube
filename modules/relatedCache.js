a1c37d90728dc8c8587a43ceafd1f9c5803282b7
sjagoori <sjagoori@users.noreply.github.com>
2021-04-17 17:40:07 +0200

    + redis function

--- modules/relatedCache.js
+++ modules/relatedCache.js
@@ -1,22 +1,62 @@
-const low = require("lowdb");
-const FileSync = require("lowdb/adapters/FileSync");
-const adapter = new FileSync("relatedCache.json");
-const db = low(adapter);
+const asyncRedis = require("async-redis");
 
 /**
- * Function set key:value pair
- * @param {String} room - key
- * @param {Object} value - value
+ * Function sets key:value pair
+ * @param {String} key key
+ * @param {String} value value
+ * @returns status code
  */
-exports.setCache = async (room, value) => {
-    db.set(room, value).write();
+exports.setCache = (key, value) => {
+    const client = asyncRedis.createClient({
+        host: process.env.REDIS_URL,
+        password: process.env.REDIS_PASS,
+        port: process.env.REDIS_PORT
+    });
+
+    return (client.set(key, value))
 }
 
 /**
- * Function gets value for given key
- * @param {String} room - key
- * @returns {Object} key's value
+ * Function gets key:value pair
+ * @param {String} key key
+ * @returns value of key
  */
-exports.getCache = (room) => {
-    return db.has(room).value() ? db.get(room).value() : undefined;
+exports.getCache = async (key) => {
+    const client = asyncRedis.createClient({
+        host: process.env.REDIS_URL,
+        password: process.env.REDIS_PASS,
+        port: process.env.REDIS_PORT
+    });
+
+    return (await client.get(key))
+}
+
+/**
+ * Function gets values for multiple keys
+ * @param {Object} keys object of keys
+ * @returns object of values
+ */
+exports.getMultiple = async (keys) => {
+    const client = asyncRedis.createClient({
+        host: process.env.REDIS_URL,
+        password: process.env.REDIS_PASS,
+        port: process.env.REDIS_PORT
+    });
+
+    return client.mget(keys)
+}
+
+/**
+ * Function deletes key:value pair
+ * @param {String} key key
+ * @returns status code
+ */
+exports.deleteCache = (key) => {
+    const client = asyncRedis.createClient({
+        host: process.env.REDIS_URL,
+        password: process.env.REDIS_PASS,
+        port: process.env.REDIS_PORT
+    });
+
+    return (client.del(key))
 }
