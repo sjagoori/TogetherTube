6defe231cfd89157e679aee5c4f9f524b8c5c7a9
sjagoori <sjagoori@users.noreply.github.com>
2021-04-18 19:32:31 +0200

    ~ formatting

--- routes/router.js
+++ routes/router.js
@@ -1,19 +1,21 @@
-const express = require('express');
+const express = require("express");
 // eslint-disable-next-line new-cap
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  return res.render('homepage')
-})
+router.get("/", (req, res) => {
+  return res.render("homepage");
+});
 
-router.get('/:videoId', (req, res) => {
-  let prep = req.params.videoId
-  return res.render('player', { prep: prep })
-})
+router.get("/:videoId", (req, res) => {
+  let prep = req.params.videoId;
+  return res.render("player", { prep: prep });
+});
 
-router.post('/handleQuery', (req, res) => {
-  let prep = req.body.query.match(/^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/)[2]
-  return res.redirect('/' + prep)
-})
+router.post("/handleQuery", (req, res) => {
+  let prep = req.body.query.match(
+    /^.*(youtu.be\/|v\/|e\/|u\/\w+\/|embed\/|v=)([^#\&\?]*).*/
+  )[2];
+  return res.redirect("/" + prep);
+});
 
-module.exports = router
+module.exports = router;
