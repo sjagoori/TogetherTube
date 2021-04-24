c3ed0ca7c79c861f4f473cdaa34385611e06dc16
sjagoori <sjagoori@users.noreply.github.com>
2021-04-24 15:56:22 +0200

    + handle API inconsistencies

--- public/scripts/related.js
+++ public/scripts/related.js
@@ -2,13 +2,20 @@ console.log('related.js running')
 
 const relatedContainer = document.getElementsByTagName('section')[0].children[1]
 
-socket.emit('getRelated', vidId)
-
 socket.on('setRelated', (emitted) => {
-if (relatedContainer.children.length == 0) emitted.items.map(key => generateCards(key.snippet.title, key.snippet.description, key.snippet.channelTitle, key.snippet.thumbnails, key.id.videoId))
+  emitted[0].items.map(key => {
+    if (key.snippet != undefined) {
+      generateCards(
+        key.snippet.title,
+        key.snippet.channelTitle,
+        key.snippet.thumbnails,
+        key.id.videoId
+      )
+    }
+  })
 })
 
-function generateCards(title, description, channelTitle, thumbnail, link) {
+function generateCards(title, channelTitle, thumbnail, link) {
   let content =
     `
     <a href=${link}>
@@ -16,7 +23,7 @@ function generateCards(title, description, channelTitle, thumbnail, link) {
     <figure>
       <div class="gradient">
         <img srcset=
-          ${thumbnail.maxres.url}  1280w,
+          ${thumbnail.maxres != undefined ? thumbnail.maxres.url : thumbnail.high.url}  1280w,
           ${thumbnail.standard.url} 640w,
           ${thumbnail.high.url} 480w,
           ${thumbnail.medium.url}  320w 
@@ -35,5 +42,3 @@ function generateCards(title, description, channelTitle, thumbnail, link) {
 
   relatedContainer.innerHTML += content
 }
-
-socket.emit('getRelated', vidId)
