c028bac063a84a7160a1ae121a9e8a6bec19dbff
sjagoori <sjagoori@users.noreply.github.com>
2021-04-24 15:55:17 +0200

    + moved events to dedicated file

--- index.js
+++ index.js
@@ -7,81 +7,17 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 
-const axios = require("axios");
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
-const messageCache = require("./modules/messageCache.js");
-const relatedCache = require("./modules/relatedCache.js");
 const router = require("./routes/router.js");
+const eventHandler = require('./modules/ioEvents.js')
 
-/**
- * * Socket.io config
- */
-io.on("connection", (socket) => {
-  socket.on("join", (room) => {
-    socket.join(room);
-    socket.broadcast.to(room).emit("userJoined");
-
-    if (io.sockets.adapter.rooms.get(room))
-      socket.emit("onlineCount", io.sockets.adapter.rooms.get(room).size);
-    if (messageCache.getCache(room) == undefined)
-      messageCache.setCache(room, []);
-  });
-
-  socket.on("state", (emitted) => {
-    io.to(emitted.room).emit("state", emitted);
-
-    emitted.playing
-      ? io.to(emitted.room).emit("playback", true)
-      : io.to(emitted.room).emit("playback", false);
-  });
-
-  socket.on("message", (message) => {
-    console.dir(message);
-    let cache = messageCache.getCache(message.room);
-    cache.push(message);
-    messageCache.setCache(message.room, cache);
-    socket.broadcast.to(message.room).emit("message", message);
-  });
-
-  socket.on("getMessages", (room) => {
-    let cache = messageCache.getCache(room);
-    let renderData = Object.values(cache)
-      .map((key) =>
-        +new Date() - key.timestamp < 3600000 && key.room == room ? key : false
-      )
-      .filter((elem) => typeof elem == "object");
-
-    io.to(room).emit("setMessages", renderData);
-  });
-
-  socket.on("getRelated", async (room) => {
-    let callString = `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${room}&type=video&key=${process.env.API_KEY}`;
-
-    if ((await relatedCache.getCache(room)) == null) {
-      await relatedCache.setCache(
-        room,
-        JSON.stringify([await axios.get(callString).then((res) => res.data)])
-      );
-      await io
-        .to(room)
-        .emit("setRelated", JSON.parse(await relatedCache.getCache(room))[0]);
-    } else {
-      io.to(room).emit(
-        "setRelated",
-        JSON.parse(await relatedCache.getCache(room))[0]
-      );
-    }
-  });
+io.on("connection", (client) => {
+  eventHandler.ioEvents(client, io)
 });
 
-/**
- * * Server config
- */
-
 app.use(express.static(path.resolve("public")));
-
 app.set("view engine", "ejs");
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
