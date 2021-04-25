9374e99ab8687ebdd22eb8355e7dff7f9f107892
sjagoori <sjagoori@users.noreply.github.com>
2021-04-25 15:15:36 +0200

    + moved worker events to seperate file

--- modules/ioEvents.js
+++ modules/ioEvents.js
@@ -1,7 +1,5 @@
-const messageCache = require("./messageCache");
-const relatedCache = require("./relatedCache.js");
-const getData = require("./getData.js");
-
+// import { sendMessagesCache, sendRelatedCache, cacheMessage } from './prepWork.js'
+const worker = require('./prepWork.js')
 /**
  * Function handles socket.io events
  * @param {Object} client - socket instance
@@ -13,10 +11,8 @@ exports.ioEvents = async (client, server) => {
     client.broadcast.to(room).emit("userJoined");
     client.emit("onlineCount", server.sockets.adapter.rooms.get(room).size);
 
-    if (messageCache.getCache(room) == undefined) messageCache.setCache(room, []);
-
-    await sendRelatedCache(room, server);
-    await sendMessagesCache(room, server);
+    await worker.sendRelatedCache(room, server);
+    await worker.sendMessagesCache(room, server);
   });
 
   client.on("state", (emitted) => {
@@ -28,50 +24,7 @@ exports.ioEvents = async (client, server) => {
   })
 
   client.on("message", (message) => {
-    cacheMessage(message);
+    worker.cacheMessage(message);
     client.broadcast.to(message.room).emit("message", message);
   });
-}
-
-/**
- * Function caches messages for given room
- * @param {Object} message - Object with message and room name
- */
-function cacheMessage(message) {
-  let cache = messageCache.getCache(message.room);
-  cache.push(message);
-  messageCache.setCache(message.room, cache);
-}
-
-/**
- * Function sends message cache to given room
- * @param {String} room - room name
- * @param {Object} server - server instance
- */
-async function sendMessagesCache(room, server) {
-  console.log(typeof server)
-  let cache = messageCache.getCache(room);
-  let renderData = Object.values(cache)
-    .map((key) =>
-      +new Date() - key.timestamp < 3600000 && key.room == room ? key : false
-    )
-    .filter((elem) => typeof elem == "object");
-
-  server.to(room).emit("setMessages", renderData);
-}
-
-/**
- * Function caches related-videos or sends cached related-videos.
- * @param {String} room - room name
- * @param {Object} server - server instance
- */
-async function sendRelatedCache(room, server) {
-  let callString = `https://www.googleapis.com/youtube/v3/search?part=snippet&relatedToVideoId=${room}&type=video&key=${process.env.API_KEY}`;
-  let cache = await relatedCache.getCache(room)
-
-  if (cache == null) {
-    await relatedCache.setCache(room, JSON.stringify([await getData.fetch(callString)]))
-  } else {
-    await server.to(room).emit("setRelated", JSON.parse(cache))
-  }
 }
