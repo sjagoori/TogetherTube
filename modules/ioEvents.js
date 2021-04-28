541f50c312160b78fc3498c28ce743d4700aa5e0
sjagoori <sjagoori@users.noreply.github.com>
2021-04-29 00:09:39 +0200

    + namespace && handle metadata

--- modules/ioEvents.js
+++ modules/ioEvents.js
@@ -1,30 +1,41 @@
-// import { sendMessagesCache, sendRelatedCache, cacheMessage } from './prepWork.js'
-const worker = require('./prepWork.js')
+const worker = require("./prepWork.js");
+const namespace = "stream/"; 
+
 /**
  * Function handles socket.io events
  * @param {Object} client - socket instance
  * @param {Object} server - io server instance
  */
 exports.ioEvents = async (client, server) => {
-  client.on("join", async (room) => {
-    client.join(room);
-    client.broadcast.to(room).emit("userJoined");
-    client.emit("onlineCount", server.sockets.adapter.rooms.get(room).size);
+  server.emit("setCurrentStreams", worker.getCurrentStreams());
 
-    await worker.sendRelatedCache(room, server);
+  client.on("join", async (room) => {
+    client.join(namespace + room);
+
+    client.broadcast.to(namespace + room).emit("userJoined");
+    client.emit(
+      "onlineCount",
+      server.sockets.adapter.rooms.get(namespace + room).size
+    );
+
+    await worker.sendRelatedCache(namespace + room, room, server);
     await worker.sendMessagesCache(room, server);
   });
 
   client.on("state", (emitted) => {
-    server.to(emitted.room).emit("state", emitted);
+    server.to(namespace + emitted.room).emit("state", emitted);
   });
 
-  client.on('playback', (state) => {
-    server.to(state.room).emit('playback', state.state)
-  })
+  client.on("playback", (state) => {
+    server.to(namespace + state.room).emit("playback", state.state);
+  });
 
   client.on("message", (message) => {
     worker.cacheMessage(message);
-    client.broadcast.to(message.room).emit("message", message);
+    client.broadcast.to(namespace + message.room).emit("message", message);
   });
-}
+
+  client.on("setMetaData", (metaData) => {
+    worker.setMetaData(metaData, server, namespace + metaData.room);
+  });
+};
